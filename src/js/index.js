5bd6f6bca6dda12dd3015aa480561f7f74ae1efd
Adrian Berenguel <adrianberenguel90@gmail.com>
2023-12-06 14:20:03 +0800

    install eslint and prettier

--- src/js/index.js
+++ src/js/index.js
@@ -3,17 +3,20 @@ import '../main.scss';
 
 const foo = (name) => {
   console.log(`Hello ${name}`);
-}
+};
+
+const test = ['a'];
 
 foo('Adrian');
 
 console.log('hello from webpack dev server');
 console.log(_.join(['Index', 'module', 'loaded!'], ' '));
+console.log(test);
 
 /**
  * * How to start the app?
  * - Execute "npm run build" or "npm run dev" in the terminal
- * 
+ *
  * - The "--open" in "webpack-dev-server --open" will open a browser window for us
  * - .join is a method from lodash. This line console.log(_.join(['Index', 'module', 'loaded!'], ' ')); basically
  *   means that it will join the array of 3 strings and separate them with a space
--- src/js/sum.js
+++ src/js/sum.js
@@ -13,6 +13,6 @@ export default sum;
 /**
  * * How to start the app?
  * - Execute "npm run build" or "npm run dev" in the terminal
- * 
+ *
  * - The "--open" in "webpack-dev-server --open" will open a browser window for us
  */
